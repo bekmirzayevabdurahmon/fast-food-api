d4230cfde6f9de3b8cf5fc23c10befdc4e1dd861
Abdurahmon <abdurahmonbekmirzayev3@gmail.com>
2025-03-29 12:01:04 +0500

    Food model finished

--- src/route/index.js
+++ src/route/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import categoryRouter from "./category.route.js";
+import foodRouter from "./food.route.js";
 
 const router = Router();
 
 router.use("/categories", categoryRouter)
+router.use("/foods", foodRouter)
 
 export default router;
